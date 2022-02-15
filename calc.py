1023acf74dcd542ff4a822b661216e7229663a74
Farukh Rakhmatullaev <joombey@yandex.ru>
2022-02-15 19:38:44 +0300

    Программный код был переписан на Объектноориентированный лад

--- calc.py
+++ calc.py
@@ -1,18 +1,19 @@
 import tkinter as tk
+from tkinter.tix import COLUMN
 
-def show(x):
-    Label = tk.Label(text = x)
-    pass
-root = tk.Tk()
-
-table = [[x for x in range(3)] for i in range(3)]
-
-Label = tk.Label(text = "0").grid(row = 0, column = 1)
-btnArr = [tk.Button(text = str(x), command = show(x)) for x in range(10)]
-btnArr.append(tk.Button(text = "="))
-index = 0
-for r in range(3):
-    for c in range(3):
-        btnArr[index].grid(column = c, row = 1 + r)
-        index +=1
-root.mainloop()
+class Calc:
+    def __init__(self, Label) -> None:
+        self.btnArr = [tk.Button(text = str(x), command = self.show) for x in range(10)]
+        self.btnArr.append([tk.Button(text = "+")])
+        self.btnArr.append([tk.Button(text = "-")])
+        self.btnArr.append([tk.Button(text = "=")])
+        self.Label = tk.Label(text = "0").grid(row = 0, column = 1)
+    LAYOUT = [[x for x in range(4)] for i in range(3)]
+    def _setButtonPos_(self):
+        for r in range(3):
+            for c in range(3):
+                self.btnArr[index].grid(column = c, row = 1 + r)
+                index +=1
+    
+    def _setLabelPos_(self):
+        self.Label.grid(row = 0, COLUMN = 4)

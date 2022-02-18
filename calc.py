8d72284c3d8b77fa78bfe72c946b6f16d4263dcf
Farukh Rakhmatullaev <joombey@yandex.ru>
2022-02-18 19:26:39 +0300

    были добавлены функции обработки кнопок

--- calc.py
+++ calc.py
@@ -1,19 +1,35 @@
 import tkinter as tk
-from tkinter.tix import COLUMN
 
 class Calc:
-    def __init__(self, Label) -> None:
-        self.btnArr = [tk.Button(text = str(x), command = self.show) for x in range(10)]
-        self.btnArr.append([tk.Button(text = "+")])
-        self.btnArr.append([tk.Button(text = "-")])
-        self.btnArr.append([tk.Button(text = "=")])
-        self.Label = tk.Label(text = "0").grid(row = 0, column = 1)
-    LAYOUT = [[x for x in range(4)] for i in range(3)]
-    def _setButtonPos_(self):
+    def __init__(self) -> None:
+        window = tk.Tk()
+
+        self.btnArr = [tk.Button(window, text = str(x), command = lambda: self.event(x)) for x in range(10)]
+        self.btnArr.append([tk.Button(window, text = "+", command = lambda: self.event("+"))])
+        self.btnArr.append([tk.Button(window, text = "-", command = lambda: self.event("-"))])
+        self.btnArr.append([tk.Button(window, text = "=", command = lambda: self.event("="))])
+        
+        self.entry = tk.Entry(window, text = "").grid(row = 0, columnspan = 4, sticky = 'wens')
+
+        self.calculator = []
+    def setButtonPos(self):
         for r in range(3):
             for c in range(3):
                 self.btnArr[index].grid(column = c, row = 1 + r)
                 index +=1
     
-    def _setLabelPos_(self):
-        self.Label.grid(row = 0, COLUMN = 4)
+    def __calc__(operation):
+        pass
+    
+    def bindButtons(self):
+        for button in self.btnArr:
+            button.bind()
+    
+    def event(self, operand):
+        if self.operand != "=":
+            self.insert(0, self.operand)
+        else:
+            self.calculator.get()
+            self.entry.delete(0, tk.END)
+
+        
